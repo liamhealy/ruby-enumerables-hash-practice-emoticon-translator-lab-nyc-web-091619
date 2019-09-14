296942f0eb63c045bd28e66ee3f545931bcd2c93
Liam Healy <lhealy0711@gmail.com>
2019-09-14 18:40:59 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,7 +21,7 @@ def get_japanese_emoticon(path, emoticon)
   if emoticon_result != nil
     return emoticon_result
   else
-    return emoticon_result = "Not a known emoticon."
+    emoticon_result = "Not a known emoticon."
   end
 end
 
